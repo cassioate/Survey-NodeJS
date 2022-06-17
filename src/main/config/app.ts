d0a8a275eb4488e08837bb378831761aa5aa3523
Cassio <cassioate@gmail.com>
2022-06-17 00:13:17 -0300

    feat: install dependencie fast-glob to use in the routes

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import setUpMiddleware from './middlewares'
+import setUpRoutes from './routes'
 
 const app = express()
 setUpMiddleware(app)
+setUpRoutes(app)
 
 export default app
---
+++ src/main/config/routes.ts
@@ -0,0 +1,10 @@
+import { Express, Router } from 'express'
+import FastGlob from 'fast-glob'
+
+export default (app: Express): void => {
+  const router = Router()
+  app.use(router)
+  FastGlob.sync('**/src/main/routes/**routes.ts').map(file => {
+
+  })
+}
