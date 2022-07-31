1c877c63b8dade8b30c9238f19567f8b003fd405
Cassio <cassioate@gmail.com>
2022-07-30 23:38:09 -0300

    chore: add swagger api configurations

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import setUpMiddleware from './middlewares'
 import setUpRoutes from './routes'
+import setupSwagger from './config-swagger'
 
 const app = express()
+setupSwagger(app)
 setUpMiddleware(app)
 setUpRoutes(app)
 
