e3bda41b855567668887efdb1854ef32fcedc4b9
Cassio <cassioate@gmail.com>
2022-07-31 00:08:42 -0300

    fix: maded corrections in names and imports necessarys

--- src/data/protocols/db/db-survey-result/save-survey-result-repository.ts
+++ src/data/protocols/db/db-survey-result/save-survey-result-repository.ts
@@ -1,5 +1,4 @@
-import { SurveyResultModel } from '../../../../domain/models/survey-result'
-import { SaveSurveyResultParams } from '../../../../domain/usecases/survey-result/save-survey-result'
+import { SaveSurveyResultParams, SurveyResultModel } from '../../../../domain/models/survey-result'
 
 export interface SaveSurveyResultRepository {
   save: (saveSurveyResult: SaveSurveyResultParams) => Promise<SurveyResultModel>
