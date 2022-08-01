7ce9f1e810b9660fc471a93d09bced855611eaf0
Cassio <cassioate@gmail.com>
2022-08-01 02:11:46 -0300

    refactor: changed the survey-result-repository return

--- src/infra/db/mongodb/survey-result-repository/survey-result-repository.ts
+++ src/infra/db/mongodb/survey-result-repository/survey-result-repository.ts
@@ -48,16 +48,3 @@ export class SurveyResultMongoRepository implements SaveSurveyResult {
     return returnSurveyResultModel
   }
 }
-
-// export type SurveyResultModel = {
-//   surveyId: string
-//   question: string
-//   answers: AnswerSurveyResultModel[]
-//   date: Date
-// }
-// export interface AnswerSurveyResultModel {
-//   image?: string
-//   answer: string
-//   count: number
-//   percent: number
-// }
